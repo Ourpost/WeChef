2be316cf2b6915ed87d71a635a02efc67b766a9b
TimWu <mr.oldfive@gmail.com>
2022-12-05 11:51:03 +0800

    store_bulid_CRUD_finsh

---
+++ app/controllers/stores_controller.rb
@@ -0,0 +1,27 @@
+class StoresController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+
+  def update
+  end
+
+  def destroy
+  end
+
+private
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  root to: "home#index"
+  resources :stores
+
+  root "stores#index"
 end
