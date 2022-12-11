10111821678403b2befdddeecbf7fdad267af4a4
TimWu <89744406+timwu0109@users.noreply.github.com>
2022-12-11 09:20:15 +0800

    已修改完成
    在user 驗證欄位中加上 :confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # devise validate
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
 
          
