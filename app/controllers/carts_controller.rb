5e021f47c07f88325e52fc6cee9f1ab57454a6b6
Yamin Xu <104242143+yamin-xu@users.noreply.github.com>
2022-12-13 11:47:33 +0800

    cart use session

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,14 +1,15 @@
 class CartsController < ApplicationController
   def add
-    render html: params
-    # current_cart.add_item(params[:id])
-    # session[Cart::SessionKey] = current_cart.serialize
+    # render html: params[:serial_number]
+    @desk = Desk.find_by(serial_number: params[:serial_number])
+    current_cart.add_item(params[:id])
+    session[Cart::SessionKey] = current_cart.serialize
 
-    # redirect_to foods_path, notice: "已加入購物車"
+    redirect_to "/foods?serial_number=#{params[:serial_number]}", notice: "已加入購物車"
   end
 
   def destroy
     session[Cart::SessionKey] = nil
-    redirect_to foods_path, notice: "購物車已清空"
+    redirect_to "/foods?serial_number=#{params[:serial_number]}", notice: "購物車已清空"
   end
 end
