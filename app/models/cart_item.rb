de4f7fc91b52eff36ce33f2fe6890e3ccffcc340
Yamin Xu <104242143+yamin-xu@users.noreply.github.com>
2022-12-14 19:41:56 +0800

    add user_id and desk_id into CartItem

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,9 +1,12 @@
 class CartItem
-  attr_reader :menu_id, :quantity
+  attr_reader :menu_id, :quantity, :desk_id, :user_id#TODO
 
-  def initialize(menu_id, quantity = 1)
+  def initialize(menu_id, quantity = 1, desk_id, user_id)#TODO
     @menu_id = menu_id
     @quantity = quantity.to_i
+    @desk_id = desk_id
+    @user_id = user_id
+    #TODO
   end
 
   def increment(n = 1)
