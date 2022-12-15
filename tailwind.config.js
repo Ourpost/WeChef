944ae5493ff820dc3136e8adb9020931fd0d5d52
TimWu <mr.oldfive@gmail.com>
2022-12-15 15:34:11 +0800

    store座位頁面新增,QRcode頁面設計

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,12 @@ const colors = require("tailwindcss/colors");
 module.exports = {
   content: [
     "./src/**/*.{html,js}",
-    "./node_modules/tw-elements/dist/js/**/*.js",
+    "./node_modules/flowbite/**/*.js",
   ],
-  plugins: [require("tw-elements/dist/plugin")],
+  plugins: [
+    require("tw-elements/dist/plugin"),
+    require('flowbite/plugin'),
+  ],
 };
 
 module.exports = {
