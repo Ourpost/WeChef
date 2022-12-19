5595d8169ca86647475f28ae9858e311f2e558f4
Yamin Xu <104242143+yamin-xu@users.noreply.github.com>
2022-12-19 17:51:40 +0800

    add aasm

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,20 @@ class Order < ApplicationRecord
   validates :serial, presence: true, uniqueness: true
   before_validation :generate_serial
 
+  include AASM
+
+  aasm do
+    state :pending, initial: true
+    state :paid, :refunded
+
+    event :pay do
+      transitions from: :pending, to: :paid
+    end
+
+    event :refund do
+      transitions from: :paid, to: :refunded
+    end
+  end
   
   private
   def generate_serial
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_17_182914) do
+ActiveRecord::Schema.define(version: 2022_12_19_094333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 2022_12_17_182914) do
     t.bigint "desk_id", null: false
     t.bigint "user_id", null: false
     t.datetime "deleted_at"
+    t.string "aasm_state"
     t.index ["deleted_at"], name: "index_orders_on_deleted_at"
     t.index ["desk_id"], name: "index_orders_on_desk_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
