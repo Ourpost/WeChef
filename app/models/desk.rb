a6847c8f5c46090347176f3ebc797b3ed0aa6ab3
TimWu <mr.oldfive@gmail.com>
2022-12-22 12:53:02 +0800

    store_destroy關聯修復可正常刪除

--- app/models/desk.rb
+++ app/models/desk.rb
@@ -3,7 +3,7 @@ class Desk < ApplicationRecord
   validates :seat_amount, presence: true
   
   belongs_to :store
-  has_many :orders
+  has_many :orders 
   
   after_create do
     self.update(serial_number: "#{rand(10000000..99999999)}".insert(3, "#{self.id}"))
