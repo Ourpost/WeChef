aa8e952e7dfd0468b42206aaf92cdd9338e26ba5
邱志壅 <jasonchiu@qiuzhiyongdeMacBook-Air.local>
2023-01-01 22:49:59 +0800

    response_controller調整

--- app/controllers/response_controller.rb
+++ app/controllers/response_controller.rb
@@ -1,11 +1,10 @@
 class ResponseController < ApplicationController
-    skip_before_action :verify_authenticity_token, only:[:index]
+  skip_before_action :verify_authenticity_token, only:[:index]
 
-    def index
-        response = Newebpay::MpgResponse.new(params[:TradeInfo])
-        @response_hash = response.result
-        @order_no = response.order_no
-        @status = response.status
-    end
-
+  def index
+    response = Newebpay::MpgResponse.new(params[:TradeInfo])
+    @response_hash = response.result
+    @order_no = response.order_no
+    @status = response.status
+  end
 end
