b86bf04dbdfd59110f14a386f31125e23194d0b5
邱志壅 <jasonchiu@qiuzhiyongdeMacBook-Air.local>
2023-01-01 22:10:25 +0800

    carts_controller調整

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,4 @@
 class CartsController < ApplicationController
-
-
   def show
     @desk = Desk.find_by(serial_number: params[:serial_number])
     @nickname = current_user ? current_user.nickname : "親愛的顧客"
@@ -8,10 +6,10 @@ class CartsController < ApplicationController
   end
 
   def add
-      @desk = Desk.find_by(serial_number: params[:serial_number])
-      current_cart.add_item(params[:id], params[:quantity])
-      session[Cart::SessionKey] = current_cart.serialize
-      redirect_to foods_path({serial_number: params[:serial_number]}), notice: "已加入購物車"
+    @desk = Desk.find_by(serial_number: params[:serial_number])
+    current_cart.add_item(params[:id], params[:quantity])
+    session[Cart::SessionKey] = current_cart.serialize
+    redirect_to foods_path({serial_number: params[:serial_number]}), notice: "已加入購物車"
   end
 
   def destroy
