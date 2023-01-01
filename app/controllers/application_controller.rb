a74a5782f22584926142997f9521c0829fd38dc1
邱志壅 <jasonchiu@qiuzhiyongdeMacBook-Air.local>
2023-01-01 22:06:24 +0800

    application_controller調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  # rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   include CartsHelper
@@ -13,23 +12,13 @@ class ApplicationController < ActionController::Base
         request.path != "/users/password/edit" &&
         request.path != "/users/confirmation" &&
         request.path != "/users/sign_out" &&
-        !request.xhr?) # don't store ajax calls
+        !request.xhr?) 
       store_location_for(:user, request.fullpath)
     end
   end
 
-
   private
-
-
-  def record_not_found
-    render file: "#{Rails.root}/public/404.html",
-           status: 404,
-           layout: false
-  end
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :role])
   end
-
 end
