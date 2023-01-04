6c332b408c184b76e8b2f1f04ed0b84d47b001fd
TimWu <mr.oldfive@gmail.com>
2023-01-04 11:48:28 +0800

    classification_index 畫面調整

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,7 +8,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: "google_oauth2") if is_navigational_format?
+      set_flash_message(:notice, :success, kind: "google") if is_navigational_format?
     else
       session["devise.google_oauth2"] = request.env["omniauth.auth"].except(:extra) # Removing extra as it can overflow some session stores
       redirect_to  user_session_path ,alert: "此信箱已申請過,請確認信箱資訊！！"
