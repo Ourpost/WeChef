ab8227c937e5320d966dc77f5b7ee7e64896445a
TimWu <mr.oldfive@gmail.com>
2023-01-06 15:01:20 +0800

    sidebar_JS修改完成

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -3,10 +3,6 @@ import { Controller } from "stimulus";
 export default class extends Controller {
   static targets = ["btn", "dropdown-btn"];
 
-  connect() {
-    console.log("我在這頁面");
-  }
-
   siderbarbtn() {
     this["dropdown-btnTarget"].classList.toggle("hidden");
     console.log(this["dropdown-btnTarget"].classList);
